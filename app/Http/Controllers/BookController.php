112b9003ca7b915cc90fb4291f602e324ac43f15
Meriliana Candra <merilianacandradevi@gmail.com>
2025-06-06 14:21:50 +0800

    Add filter on Book Controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 use stdClass;
 
 class BookController extends Controller
 {
     public function index()
     {
-        $books = Book::get();
-        return JsonResource::collection($books); // collection for more than one data -> json
+        // $books = Book::get();
         // return view('index', ['books'=>$books]);
+
+        // GET /users?filter[name]=john&filter[email]=gmail
+
+        $books = QueryBuilder::for(Book::class)
+            ->allowedFilters([AllowedFilter::exact('is_available')])
+            ->get();
+
+        // $users will contain all users with "john" in their name AND "gmail" in their email address
+        return JsonResource::collection($books); // collection for more than one data -> json
     }
 
     //(Request $request) => only need if FE sent the payload
@@ -27,11 +37,11 @@ class BookController extends Controller
 
         $book = Book::create($validated);
         return JsonResource::make($book); // make for one data -> json
-        
+
     }
 
     public function update(Request $request, Book $book)
-    {        
+    {
         $validated = $request->validate([
             'judul' => 'required',
             'penulis' => 'required',
